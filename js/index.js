c06466cb9d6330e55882b8c24bf0d61c048eda4b
wntiv-main <60457971+wntiv-main@users.noreply.github.com>
2023-08-17 14:10:47 +1200

    fix alignment

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
 function initParallax() {
+    var navHeight = document.getElementsByClassName("pre-nav")[0].getBoundingClientRect().height + document.getElementsByClassName("navbar")[0].getBoundingClientRect().height;
     for(var el of document.getElementsByClassName("parallax-container")) {
         el.firstElementChild.style.height = (el.lastElementChild.offsetTop + el.lastElementChild.getBoundingClientRect().height / 2 + window.innerHeight / 2) + "px";
     }
     for(var el of document.getElementsByClassName("parallax-object")) {
-        el.style.setProperty("--center-on", (el.getBoundingClientRect().height / 2 - el.parentElement.getBoundingClientRect().height) + "px");
-        el.style.setProperty("--left-offset", (el.getBoundingClientRect().left - parseFloat(el.style.getPropertyValue("--left-offset") || 0)) + "px")
+        el.style.setProperty("--center-on", (el.getBoundingClientRect().height / 2 - el.parentElement.getBoundingClientRect().height + navHeight) + "px");
+        var style = window.getComputedStyle(el, null);
+        var scale = parseFloat(style.getPropertyValue("--z-offset")) / parseFloat(style.getPropertyValue("--perspective"));
+        el.style.setProperty("--left-offset", `${(el.getBoundingClientRect().left + parseFloat(style.getPropertyValue("--left-offset") || 0))}px`)
     }
 }
 
